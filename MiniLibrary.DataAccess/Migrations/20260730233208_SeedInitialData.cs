a925c298ec7f8cd1e6bfc942723d3b9f102c50fe
ZeyadWael32 <zozowael932006@gmail.com>
2026-07-31 01:33:03 +0200

    feat: add initial data migration

---
+++ MiniLibrary.DataAccess/Migrations/20260730233208_SeedInitialData.cs
@@ -0,0 +1,217 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MiniLibrary.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitialData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "Id", "Biography", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, "John Doe is a prolific author known for his thrilling novels.", "John", "Doe" },
+                    { 2, "Jane Smith is a renowned author who has written numerous bestsellers in the romance genre.", "Jane", "Smith" },
+                    { 3, "Michael Johnson is an award-winning author specializing in historical fiction.", "Michael", "Johnson" },
+                    { 4, "Emily Davis is a talented author known for her captivating fantasy novels.", "Emily", "Davis" },
+                    { 5, "David Wilson is a versatile author who has written across various genres, including mystery and thriller.", "David", "Wilson" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Fiction" },
+                    { 2, "Non-Fiction" },
+                    { 3, "Science" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Publishers",
+                columns: new[] { "Id", "Address", "Name" },
+                values: new object[,]
+                {
+                    { 1, "123 Main St", "Publisher 1" },
+                    { 2, "456 Oak Ave", "Publisher 2" },
+                    { 3, "789 Pine Rd", "Publisher 3" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "CategoryId", "Description", "ISBN", "Price", "PublisherId", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, null, "1234567890", 9.99m, 1, "Book 1" },
+                    { 2, 2, null, "0987654321", 14.99m, 1, "Book 2" },
+                    { 3, 3, null, "5678901234", 19.99m, 2, "Book 3" },
+                    { 4, 1, null, "9876543210", 24.99m, 3, "Book 4" },
+                    { 5, 2, null, "4321098765", 29.99m, 3, "Book 5" },
+                    { 6, 3, null, "2468135790", 12.99m, 2, "Book 6" },
+                    { 7, 1, null, "1357924680", 17.99m, 1, "Book 7" },
+                    { 8, 2, null, "8642097531", 22.99m, 3, "Book 8" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AuthorBook",
+                columns: new[] { "AuthorsId", "BooksId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 1, 6 },
+                    { 2, 2 },
+                    { 2, 7 },
+                    { 3, 3 },
+                    { 3, 8 },
+                    { 4, 4 },
+                    { 5, 5 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AuthorBook",
+                keyColumns: new[] { "AuthorsId", "BooksId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "AuthorBook",
+                keyColumns: new[] { "AuthorsId", "BooksId" },
+                keyValues: new object[] { 1, 6 });
+
+            migrationBuilder.DeleteData(
+                table: "AuthorBook",
+                keyColumns: new[] { "AuthorsId", "BooksId" },
+                keyValues: new object[] { 2, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "AuthorBook",
+                keyColumns: new[] { "AuthorsId", "BooksId" },
+                keyValues: new object[] { 2, 7 });
+
+            migrationBuilder.DeleteData(
+                table: "AuthorBook",
+                keyColumns: new[] { "AuthorsId", "BooksId" },
+                keyValues: new object[] { 3, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "AuthorBook",
+                keyColumns: new[] { "AuthorsId", "BooksId" },
+                keyValues: new object[] { 3, 8 });
+
+            migrationBuilder.DeleteData(
+                table: "AuthorBook",
+                keyColumns: new[] { "AuthorsId", "BooksId" },
+                keyValues: new object[] { 4, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "AuthorBook",
+                keyColumns: new[] { "AuthorsId", "BooksId" },
+                keyValues: new object[] { 5, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Authors",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Publishers",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
